0c2a516d0ddd7f0664e68c5cb394f0ab66693cf4
sogrey <suzhaoheng20016@163.com>
2017-06-20 11:15:30 +0800

    update 2.1

---
+++ source/P02/src/org/sogrey/kotlin/demo_2_1.java
@@ -0,0 +1,7 @@
+package org.sogrey.demo;
+
+/**
+ * Created by Sogrey on 2017/6/20.
+ */
+public class demo_2_1 {
+}
---
+++ source/P02/src/org/sogrey/kotlin/demo_2_1.kt
@@ -0,0 +1,8 @@
+package org.sogrey.demo
+
+/**
+ * Created by Sogrey on 2017/6/20.
+ */
+fun main(args: Array<String>) {
+    println("Hello Kotlin.")
+}
